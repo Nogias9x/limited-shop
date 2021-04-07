79cb9937fab957dea4ee20ce7fe79c6c287b1ce1
Tu Nguyen <nogias9x@gmail.com>
2021-04-07 11:58:20 +0700

    Add fallback port 4444444444

--- src/server/middleware/server.js
+++ src/server/middleware/server.js
@@ -3,11 +3,20 @@
 import Promise from 'bluebird';
 import expressConfig from './express';
 
+const xxx = () => {
+    const a = 123;
+    const b= 456;
+    return a===b;
+}
+
 export default (app) => {
 	return new Promise((resolve) => {
-		expressConfig(app);
+		console.log('start server');
+                expressConfig(app);
 
-		app.listen(app.get('port'), () => {
+                xxx();
+
+		app.listen(app.get('port') || 3000, () => {
 			console.log('START LISTEN PORT - ', app.get('port'));
 			resolve();
 		});
